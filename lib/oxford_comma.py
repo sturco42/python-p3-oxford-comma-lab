4f748378cbd540788fc5ec82eac6e001b66d8a93
Sam <samturco42@gmail.com>
2023-05-30 12:39:23 -0700

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
+items = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+
 def oxford_comma(items):
-    return None
+    if len(items) > 1:
+        items[-1] = "and " + items[-1]
+
+    if len(items) > 2:
+        return ', '.join(items)
+    else:
+        return ' '.join(items)
+print(oxford_comma(items))
